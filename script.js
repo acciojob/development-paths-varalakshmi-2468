6510e91926e711a6d0a7755e007e79c17d1144dc
varalakshmi-2468 <118335897+varalakshmi-2468@users.noreply.github.com>
2025-09-17 22:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+// Simple JavaScript functionality
+document.getElementById("alertBtn").addEventListener("click", function () {
+  alert("Welcome to Accio Jobs Development Paths!");
+});
+
